2ddcc53d4336c3fc2c7bc8dc90a05235855f882f
Owen Adirah <owenadira@gmail.com>
2023-07-27 16:31:15 +0300

    frontend  skeleton

---
+++ soko-bora-web-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'soko-bora-web-app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'soko-bora-web-app';
+}
