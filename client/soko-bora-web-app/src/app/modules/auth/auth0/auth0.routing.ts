353315299a5975de76f05644171990db3bf24e4b
Owen Adirah <owenadira@gmail.com>
2023-08-27 00:42:14 +0300

    create enviroment files

---
+++ client/soko-bora-web-app/src/app/modules/auth/auth0/auth0.routing.ts
@@ -0,0 +1,9 @@
+import { Route } from '@angular/router';
+import {Auth0Component} from './auth0.component';
+
+export const auth0Routes: Route[] = [
+    {
+        path     : '',
+        component: Auth0Component
+    }
+];
