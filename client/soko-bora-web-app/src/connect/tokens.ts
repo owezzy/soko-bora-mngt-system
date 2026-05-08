8fef32aea356726e271e52d8df708014164da2dd
Owen Adirah <owenadira@gmail.com>
2026-05-09 01:44:53 +0300

    add kiosk connect client wiring
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- client/soko-bora-web-app/src/connect/tokens.ts
+++ client/soko-bora-web-app/src/connect/tokens.ts
@@ -1,8 +1,14 @@
 import {createClientToken} from "./connect.module";
+import {BasketService} from '../proto/basketspb/api_pb';
 import {CustomersService} from "../proto/customerspb/api_pb";
+import {OrderingService} from '../proto/orderingpb/api_pb';
+import {PaymentsService} from '../proto/paymentspb/api_pb';
 import {SearchService} from '../proto/searchpb/api_pb';
 
+export const BasketGrpcService = createClientToken(BasketService);
 export const CustomerGrpcService = createClientToken(CustomersService);
+export const OrderingGrpcService = createClientToken(OrderingService);
+export const PaymentsGrpcService = createClientToken(PaymentsService);
 export const SearchGrpcService = createClientToken(SearchService);
 
 // Additional client tokens representing Connect services could be added here
